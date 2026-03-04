b46492232873be72bfdf1692dbe64149f0aa04df
Velpula Lakshmi Nikitha <velpulalakshminikitha@gmail.com>
2026-03-04 17:26:44 +0530

    Initial commit - safe code only

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://PlaceO:8xSMFwcD7qF5lBnJ@placeo.2jgd051.mongodb.net/PlaceO"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed", error.message);
